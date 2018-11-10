9394c3f96f862946cb393f0529f4aed25ede9352
Giovanni Aguirre <giovanni.fi05@gmail.com>
2018-11-09 22:36:40 -0600

    :rocket: Add suggestion to add composer script as an alias

--- DevEnvironments/Php/composer.sh
+++ DevEnvironments/Php/composer.sh
@@ -5,5 +5,8 @@
 # script as if it be the composer binary, For example:
 # sh ./composer.sh create-project --prefer-dist laravel/laravel MYAPP
 #
+# Consider add this script as an alias in ~/bash_aliases:
+# alias composer='sh </path/to/scripts/composer.sh>'
+#
 # For advanced documentation check: https://hub.docker.com/_/composer/
 docker run -ti --rm -v $PWD:/app composer:1.7.3 $@

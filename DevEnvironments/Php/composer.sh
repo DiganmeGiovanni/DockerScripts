a69bea504132ec7020dbdd122b10f055c7085448
Giovanni Aguirre <giovanni.fi05@gmail.com>
2018-11-09 22:35:16 -0600

    :rocket: Add script to execute fixed composer version 1.7.3

--- DevEnvironments/Php/composer.sh
+++ DevEnvironments/Php/composer.sh
@@ -1,17 +1,9 @@
 #!/bin/sh
 
-##
-## This is the original script taken from https://hub.docker.com/r/composer/composer/
-
-# export PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin
-# echo "Current working directory: '"$(pwd)"'"
-# docker run --rm -v $(pwd):/app -v ~/.ssh:/root/.ssh composer/composer $@
-
-##
-## Instead I only use:
-docker run --rm -v /home/giovanni/Sources/Php/TEAProjectManager:/app -v ~/.ssh:/root/.ssh composer/composer $@
-
-## 
-## Now execute this script as if it be the composer binary
-## For example:
-## sh ./composer.sh create-project --prefer-dist laravel/laravel MYAPP
+#
+# Starts a docker image with composer included, you can use this
+# script as if it be the composer binary, For example:
+# sh ./composer.sh create-project --prefer-dist laravel/laravel MYAPP
+#
+# For advanced documentation check: https://hub.docker.com/_/composer/
+docker run -ti --rm -v $PWD:/app composer:1.7.3 $@

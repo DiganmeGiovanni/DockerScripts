552459c5bdc251de3864dbd35fc0fc7281a275f6
Giovanni Aguirre <--global>
2019-05-23 22:17:42 -0500

    :rocket: Fix mysql client launcher

--- DevEnvironments/MySQL/mysql_client.sh
+++ DevEnvironments/MySQL/mysql_client.sh
@@ -4,4 +4,4 @@
 # Checkout: https://hub.docker.com/_/mysql for extra info
 #
 # Tip: Setup an alias to this script to use from command line
-docker run -it --rm mysql $@
+docker run -it --rm mysql mysql $@

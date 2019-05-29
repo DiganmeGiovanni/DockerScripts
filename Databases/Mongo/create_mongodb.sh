8c7b39e56d2c22dc952ef42f202e4ef0c84ccb38
Giovanni Aguirre <--global>
2019-05-28 21:39:40 -0500

    :rocket: Update create mongo script to use official image

--- Databases/Mongo/create_mongodb.sh
+++ Databases/Mongo/create_mongodb.sh
@@ -4,7 +4,7 @@
 # Created on: Sat, Jun 13, 2015
 # @author Giovanni Aguire | @DiganmeGiovanni
 #
-# Create and run a mongodb container based on image 'tutum/mongodb:3.0'
+# Create and run a mongodb container based on official image
 # -------------------------------------------------------------------------
 #
 # 1. Ask to the user for a name for container
@@ -17,6 +17,7 @@ CONTAINER_NAME="mongodb"
 LOCAL_MONGODB_PORT="27017"
 LOCAL_HTTPINTERFACE_PORT="28017"
 MONGODB_PASSWORD="root"
+DOCKER_IMAGE_TAG="latest"
 
 # Asking for container's name
 echo
@@ -58,12 +59,40 @@ then
     MONGODB_PASSWORD=$TMP_PASSWORD
 fi
 
+# Ask for docker image tag
+echo
+echo "Which docker image tag should the container use?"
+read -p "Docker image tag [latest]: " TMP_TAG
+if ! [ -z "$TMP_TAG" ]; then
+    DOCKER_IMAGE_TAG=$TMP_TAG
+fi
+echo "Using image tag [$DOCKER_IMAGE_TAG]"
+
+# Ask for custom data dir
+echo
+echo "Indicate a local route where to store data (If empty data will be stored on container)"
+read -p " Data directory: " DATA_DIR
+if ! [ -z "$DATA_DIR" ];
+then
+    CUSTOM_DATA_DIR=true
+fi
+
+## Assemble command
+DOCKER_COMMAND="docker run -ti  -p $LOCAL_MONGODB_PORT:27017 "
+DOCKER_COMMAND="$DOCKER_COMMAND -p $LOCAL_HTTPINTERFACE_PORT:28017 "
+DOCKER_COMMAND="$DOCKER_COMMAND --name $CONTAINER_NAME "
+if $CUSTOM_DATA_DIR ; then
+    DOCKER_COMMAND="$DOCKER_COMMAND -v $DATA_DIR:/data/db"
+fi
+DOCKER_COMMAND="$DOCKER_COMMAND -e MONGO_INITDB_ROOT_USERNAME=root "
+DOCKER_COMMAND="$DOCKER_COMMAND -e MONGO_INITDB_ROOT_PASSWORD=$MONGODB_PASSWORD"
+DOCKER_COMMAND="$DOCKER_COMMAND -d mongo:$DOCKER_IMAGE_TAG"
+
 ## Create and start container
 echo
-echo
 echo "Creating container with command:"
-echo "docker run -t -i -p $LOCAL_MONGODB_PORT:27017 -p $LOCAL_HTTPINTERFACE_PORT:28017 --name $CONTAINER_NAME -e MONGODB_PASS=$MONGODB_PASSWORD tutum/mongodb:3.0"
+echo $DOCKER_COMMAND
+eval $DOCKER_COMMAND 
 
 echo
-echo "executing command ..."
-docker run -t -i -p $LOCAL_MONGODB_PORT:27017 -p $LOCAL_HTTPINTERFACE_PORT:28017 --name $CONTAINER_NAME -e MONGODB_PASS=$MONGODB_PASSWORD tutum/mongodb:3.0 /bin/bash
+echo "Container start successfully"
